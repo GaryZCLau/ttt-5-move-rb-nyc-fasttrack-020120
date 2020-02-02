830ea1ec4f2aeb743a3d38d96911832c7a8e7eff
benign-fork-5056 <gareeeliu@gmail.com>
2020-02-02 22:15:07 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,24 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  new_input = input.to_i - 1
+  return new_input
+end
+
+def move(board, index, token = "X")
+  board[index] = token
+  return board
+end
+
 # code your input_to_index and move method here!
+# notes from class
+# def move(board, index, token)
+#   board[index] = token
+# end
+#
+# def input_to_index
+#   puts "Please enter 1-9:"
+#   input = gets.strip
+#   input.to_i - 1
+# end
